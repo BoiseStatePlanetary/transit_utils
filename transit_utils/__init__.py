edf0c4f7dff48fb9f4830ed872505e68ddc76512
Brian Jackson <bjackson@boisestate.edu>
2018-06-04 15:17:18 -0600

    Added template

---
+++ transit_utils/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ['transit_utils']
+  
+from .transit_utils import *
---
+++ transit_utils/transit_utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def calc_phi(time, params):
+    """
+    Calculates orbital phase
+
+    Parameters
+    ----------
+    """
